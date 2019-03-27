d82c1f2ecb496de35a704a351dd2dc6a88438b64
Apoorv Joshi <apoorv@binary.com>
2019-03-27 15:04:49 +0800

    Updates

---
+++ pages-manifest-9202fc0ed294f82fb37d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{138:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","sq--src-components-seo-js":1025518380,"sq--src-components-styled-image-js":2840059575}}}}]);
+//# sourceMappingURL=pages-manifest-9202fc0ed294f82fb37d.js.map
